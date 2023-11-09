b490b6859eb9c7ecb1d60407ee56f57c883655cc
Rogelio <149198850+rogeermv@users.noreply.github.com>
2023-11-08 18:15:55 -0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -8,13 +8,13 @@ segments = [ 0, 112, 63, 111, 121, 56, 48, 63 ]
 @cocotb.test()
 async def test_7seg(dut):
     dut._log.info("start")
-    clock = Clock(dut.clk, 10, units="us")
+    clock = Clock(dut.clk, 1, units="ms")
     cocotb.start_soon(clock.start())
 
     # reset
     dut._log.info("reset")
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 1)
     dut.rst_n.value = 1
 
     dut._log.info("check all segments")
